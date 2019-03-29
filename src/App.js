1115c260fd80104d903be423eb9d652b7373c7e9
grantposell <grantposell@gmail.com>
2019-03-28 23:08:49 -0500

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from "./components/main";
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header-color" title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">ChargeShare</Link>} scroll>
+            <Navigation>
+              <a href="/invest">Invest</a>
+              <a href="/network">Network</a>
+              <a href="/pricing">Pricing</a>
+              <a href="/aboutus">About Us</a>
+            </Navigation>
+          </Header>
+          <Drawer title={<Link style={{textDecoration:'none', color: 'black'}} to="/">ChargeShare</Link>}>
+            <Navigation>
+              <a href="/invest">Invest</a>
+              <a href="/network">Network</a>
+              <a href="/pricing">Pricing</a>
+              <a href="/aboutus">About Us</a>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+          </Content>
+        </Layout>
       </div>
     );
   }
---
+++ src/components/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import LandingPage from './landingpage';
+import Invest from './invest';
+import Network from './network';
+import Pricing from './pricing';
+import AboutUs from './AboutUs';
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component = {LandingPage} />
+        <Route path="/invest" component = {Invest} />
+        <Route path="/network" component = {Network} />
+        <Route path="/pricing" component = {Pricing} />
+        <Route path="/aboutus" component = {AboutUs} />
+    </Switch>
+)
+
+export default Main;
