5de3595e82a45b280e2e533951bca793d0038adb
grantposell <grantposell@gmail.com>
2019-03-29 18:31:12 -0500

    Added footer to every page but landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Layout, Header, Navigation, Drawer, Content, Footer, FooterDropDownSection, FooterSection, FooterLinkList } from 'react-mdl';
 import Main from "./components/main";
 import { Link } from 'react-router-dom';
 
@@ -17,7 +17,7 @@ class App extends Component {
               <a href="/aboutus">About Us</a>
             </Navigation>
           </Header>
-          <Drawer title={<Link style={{textDecoration:'none', color: 'black'}} to="/">ChargeShare</Link>}>
+          <Drawer title={<Link style={{ textDecoration: 'none', color: 'black' }} to="/">ChargeShare</Link>}>
             <Navigation>
               <a href="/invest">Invest</a>
               <a href="/network">Network</a>
@@ -29,6 +29,41 @@ class App extends Component {
             <div className="page-content" />
             <Main></Main>
           </Content>
+          <Footer size="mega">
+            <FooterSection type="middle">
+              <FooterDropDownSection title="Features">
+                <FooterLinkList>
+                  <a href="#">About</a>
+                  <a href="#">Terms</a>
+                  <a href="#">Partners</a>
+                  <a href="#">Updates</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="Details">
+                <FooterLinkList>
+                  <a href="#">Specs</a>
+                  <a href="#">Tools</a>
+                  <a href="#">Resources</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="Technology">
+                <FooterLinkList>
+                  <a href="#">How it works</a>
+                  <a href="#">Patterns</a>
+                  <a href="#">Usage</a>
+                  <a href="#">Products</a>
+                  <a href="#">Contracts</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="FAQ">
+                <FooterLinkList>
+                  <a href="#">Questions</a>
+                  <a href="#">Answers</a>
+                  <a href="#">Contact Us</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+            </FooterSection>
+          </Footer>
         </Layout>
       </div>
     );
---
+++ src/components/pricing.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+import { Grid, Cell } from 'react-mdl';
+
+class Pricing extends Component {
+    render() {
+        return (
+        <Grid>
+            <Cell col={4}>
+            <p>
+            </p>
+            </Cell>
+            <Cell col={8}>
+            <p>
+            </p>
+            </Cell>
+        </Grid>
+        )
+    }
+}
+export default Pricing;
