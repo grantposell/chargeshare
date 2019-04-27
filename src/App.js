de30d9df0f45ab3805918e2c1b18960746b6953a
grantposell <grantposell@gmail.com>
2019-04-26 22:40:09 -0500

    Altered nav

--- src/App.js
+++ src/App.js
@@ -12,10 +12,9 @@ class App extends Component {
         <Layout>
           <Header className="header-color" title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">ChargeShare</Link>} scroll>
             <Navigation>
-              <a href="/invest">Invest</a>
+              <a href="/charging">Charging</a>
+              <a href="/storage">Energy</a>
               <a href="/network">Network</a>
-              <a href="/pricing">Pricing</a>
-              <a href="/aboutus">About Us</a>
             </Navigation>
           </Header>
           <Drawer title={<Link style={{ textDecoration: 'none', color: 'black' }} to="/">ChargeShare</Link>}>
